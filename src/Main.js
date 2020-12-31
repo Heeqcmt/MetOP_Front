be5043ed278b9ce5bc34652523288da831271aad
YuMing Zhang <yumingzhang94@gmail.com>
2020-12-31 00:41:16 -0500

    pushing event array up

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,106 @@
 import React from "react";
 import Selector from "./mainComponent/selector";
 import EventBoard from "./mainComponent/eventBoard";
+//sample data
+const testEventArray = [
+    {
+        title: "Liberal party event 1",
+        id: 0,
+        location: "online",
+        province: "ontario",
+        party: "liberal",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+    },
+    {
+        title: "Conservative party event 1",
+        id: 1,
+        location: "online",
+        province: "ontario",
+        party: "conservative",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    },
+    {
+        title: "NDP event 1",
+        id: 2,
+        location: "online",
+        province: "ontario",
+        party: "NDP",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    },
+    {
+        title: "Liberal party event 1",
+        id: 0,
+        location: "online",
+        province: "ontario",
+        party: "liberal",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+    },
+    {
+        title: "Conservative party event 1",
+        id: 1,
+        location: "online",
+        province: "ontario",
+        party: "conservative",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    },
+    {
+        title: "NDP event 1",
+        id: 2,
+        location: "online",
+        province: "ontario",
+        party: "NDP",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    },
+    {
+        title: "Liberal party event 1",
+        id: 0,
+        location: "online",
+        province: "ontario",
+        party: "liberal",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+    },
+    {
+        title: "Conservative party event 1",
+        id: 1,
+        location: "online",
+        province: "ontario",
+        party: "conservative",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    },
+    {
+        title: "NDP event 1",
+        id: 2,
+        location: "online",
+        province: "ontario",
+        party: "NDP",
+        link: "sample link",
+        date: "sampel date",
+        description: "sample description",
+
+    }
+
+];
 export default class Main extends React.Component{
     
         render()
@@ -8,7 +108,7 @@ export default class Main extends React.Component{
             return(
                 <>
                 < Selector />
-                < EventBoard />
+                < EventBoard eventArray ={testEventArray}/>
                 </>
             );
         };
