38b9d2db1939d731f2a19ee20658d1f0dd26ed71
YuMing Zhang <yumingzhang94@gmail.com>
2021-01-04 14:15:33 -0500

    added link to the card

--- src/mainComponent/eventBoard.js
+++ src/mainComponent/eventBoard.js
@@ -18,16 +18,19 @@ export default class EventBoard extends React.Component {
                             this.props.eventArray.map(
                                 event => (
                                     <Col md="4" >
-                                        <Card bg='light' > 
+                                        <a style={{cursor:'pointer'}} onClick={()=>window.open(event.Info.link,"_blank")}>
+                                        <Card bg='light'> 
                                             <Card.Header>{event.Info.title}</Card.Header>
-                                            <Card.Body>
+                                            <Card.Body >
                                                 <Card.Text>
                                                     <div>{event.Info.party}</div>
                                                     <div>{event.province}</div>
                                                     <div>{event.Info.date}</div>
                                                 </Card.Text>
+                            
                                             </Card.Body>
                                         </Card>
+                                        </a>
                                     </Col>
 
                                 )
