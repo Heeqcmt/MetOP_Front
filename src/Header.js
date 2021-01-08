adc70fe94644986885b9111223ead8c25b23677e
YuMing Zhang <yumingzhang94@gmail.com>
2021-01-07 19:31:37 -0500

    added logo thanks to Vicky Qin

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react-dom';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { Container } from 'react-bootstrap';
+import logo from './logo/logo.png';
+import './index.css'
 
 export default function Header() {
     
@@ -8,12 +10,14 @@ export default function Header() {
             <header>
                 <Jumbotron>
                     <Container>
+
+                        
                         <h1>
-                            Meet the Ontario Parites
+                        <img className="logo"src={logo} ></img> Meet the Ontario Parties
                         </h1>
-                        <p>
-                            One place for all the events posted by Ontario Parties.
-                        </p>
+                        <pre>
+                                            One place for all the events posted by Ontario Parties.
+                        </pre>
                     </Container>
 
                 </Jumbotron>
